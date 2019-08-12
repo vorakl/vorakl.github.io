7b6048244a6e19a33dfc50e7edc1638e02a6a433
Oleksii Tsvietnov <vorakl@protonmail.com>
2019-08-12 15:31:15 +0200

    New content

--- src/siteprod.py
+++ src/siteprod.py
@@ -15,5 +15,5 @@ from sitedev import *
 RELATIVE_URLS = False
 #GOOGLE_ANALYTICS = 'UA-142190768-1'
 FEED_EMAIL = 'https://feedburner.google.com/fb/a/mailverify?uri=voraklsnotes/atom&amp;loc=en_US'
-FEED_DOMAIN = 'http://feed.vorakl.com'
+FEED_DOMAIN = 'https://feeds.feedburner.com'
 FEED_ALL_ATOM = 'voraklsnotes/atom'
